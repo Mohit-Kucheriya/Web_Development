b69c12bfe274458c712fe7c2ff5b69d153e2be34
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-10-10 21:25:40 +0530

    04_operations

---
+++ JS/js-basics-01/04_operations.js
@@ -0,0 +1,56 @@
+// Addition
+let add = 4 + 5;
+let num1 = 100;
+let num2 = 102;
+console.log(add);
+console.log("Addition :", num1 + num2);
+
+// Subtraction
+let sub1 = 102;
+let sub2 = 12;
+console.log("Subtraction :", sub1 - sub2);
+
+// Multiplication
+let mul1 = 154;
+let mul2 = 13;
+console.log("Multiplication :", mul1 * mul2);
+
+// Division
+let div1 = 10 / 8;
+console.log("Division :", div1);
+
+// Remainder
+let rem = 54 % 3;
+console.log("Remainder :", rem);
+
+// Exponentation
+let expo = 2 ** 5;
+console.log("Exponentation :", expo);
+
+// Comparison operations
+/*
+1. == : comparison operator which check wether the value is same or not, and give the boolean result i.e true or false
+
+2. != : not equalTo 
+
+3. < : less than
+
+4. > : greater than
+
+*/
+let comp1 = 15;
+let comp2 = 15;
+let comp3 = 65;
+
+console.log(comp1 == comp2);
+console.log(comp1 != comp3);
+
+// Operator precedence defines the order in which operations are performed when there are multiple operators in an expression.
+
+let operationPres = ((2 * (5 + 1)) - 5);
+let operationPres1 = (2 * ((5 + 1) - 5));
+
+console.log(operationPres);
+console.log(operationPres1);
+
+
