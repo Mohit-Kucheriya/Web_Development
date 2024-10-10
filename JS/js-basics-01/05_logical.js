d3c4299c4c4e262b8dbe331ac65ad974ef205bcc
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-10-10 21:26:14 +0530

    05_logical

---
+++ JS/js-basics-01/05_logical.js
@@ -0,0 +1,20 @@
+/*
+1. && : Logical AND, give boolean result i.e if both condition are true then only true .
+ 
+2. || : Logical OR, give boolean result i.e any one needs to be true.
+
+3. ! : reversed the result. i.e if false -> true and true -> false
+
+*/
+
+// Logical AND
+let isLoggedIn = true;
+let purchaseCourse = true;
+
+console.log(isLoggedIn && purchaseCourse);
+
+// Logical OR
+let emailUser = true;
+let googleUser = false;
+
+console.log(emailUser || googleUser);
