fdc06bec49997b9ff247744e6c69ff65a3e2bdaa
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-10-10 21:24:39 +0530

    02_printing

---
+++ JS/js-basics-01/02_printing.js
@@ -0,0 +1,5 @@
+console.log("Heloo World");
+console.warn("Hey this is the warning");
+process.stdout.write("JavaScript") // Different way to print the data, but it's get continue on the same line
+
+
