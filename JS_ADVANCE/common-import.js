0404e8f93594e7c4b4a40f30ea165b34d68336c9
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-10-23 22:41:51 +0530

    common-import

---
+++ JS_ADVANCE/common-import.js
@@ -0,0 +1,6 @@
+// CommonJS
+
+let commonImport = require("./common-export.js")
+console.log(commonImport.add(2, 3))
+console.log(commonImport.subtract(2, 3))
+console.log(commonImport.multiply(2, 3))    
