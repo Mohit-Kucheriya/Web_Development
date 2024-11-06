92d13d901044ee80c95e6c7532dc69674e66aceb
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-11-06 17:27:00 +0530

    Quiz_Application

--- JS_PROJECTS/Quiz_Application/script.js
+++ JS_PROJECTS/Quiz_Application/script.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-
     const startButton = document.getElementById("start-btn");
     const restartButton = document.getElementById("restart-btn");
     const nextButton = document.getElementById("next-btn");
-    const questionContainerDisplay = document.getElementById("question-container");
+
+    const questionContainerDisplay =
+        document.getElementById("question-container");
+    const resultContainerDisplay = document.getElementById("result-container");
+
     const questionText = document.getElementById("question-text");
     const choicesList = document.getElementById("choices-list");
-    const resultContainerDisplay = document.getElementById("result-container")
     const scoreDisplay = document.getElementById("score");
 
     const quizData = [
@@ -42,7 +44,69 @@ document.addEventListener("DOMContentLoaded", () => {
         },
     ];
 
+    // Always remember to keep the track of the current question index, so that we can access the correct question data
+    let currentQuestionIndex = 0;
+
+    // Initialize the score to 0
+    let score = 0;
+
+    // Whenever we pass any method, always pass the reference of it, so that when the user clicks on the button, the method is called.
+    startButton.addEventListener("click", startQuiz);
+
+    nextButton.addEventListener("click", () => {
+        currentQuestionIndex++;
+
+        if (currentQuestionIndex < quizData.length) {
+            showQuestion();
+        } else {
+            showResult()
+        }
 
 
+    })
 
+    restartButton.addEventListener("click", () => {
+        currentQuestionIndex = 0; // Set the currentQuestionIndex to 0
+        score = 0; // Set the score to 0
+        resultContainerDisplay.classList.add("hidden");
+        startQuiz();
+    })
+
+    function startQuiz() {
+        // As soon as user clicks on the startButton, first of all hide the startButton, resultContainerDisplay should be hidden, and questionContainerDisplay should be visible.
+        startButton.classList.add("hidden");
+        resultContainerDisplay.classList.add("hidden");
+        questionContainerDisplay.classList.remove("hidden");
+
+        // Now, we need to show the questionText and choicesList, so that the user can see the question and the options.
+        showQuestion();
+    }
+
+    function showQuestion() {
+        nextButton.classList.add("hidden");
+        questionText.textContent = quizData[currentQuestionIndex].question;
+        choicesList.innerHTML = ""; // Clear the previous choicesList
+        quizData[currentQuestionIndex].options.forEach((option) => {
+            const li = document.createElement("li");
+            li.textContent = option;
+            li.addEventListener("click", () => selectAnswer(option)); // Whenever the user clicks on any option, we need to call the selectAnswer method with the option as an argument, if we pass the method as selectAnswer(option), it will execute it immediately, so wrap it inside a callback function.
+            choicesList.appendChild(li);
+        });
+    }
+
+    function selectAnswer(option) {
+        // Whenever the user clicks on any option, we need to check if the option is correct or not, if it is correct, we need to increment the score.
+        const correctAnswer = quizData[currentQuestionIndex].answer;
+        if (option === correctAnswer) {
+            score++;
+        }
+        nextButton.classList.remove("hidden");
+    }
+
+    function showResult() {
+        questionContainerDisplay.classList.add("hidden");
+        resultContainerDisplay.classList.remove("hidden");
+        scoreDisplay.textContent = `${score} out of ${quizData.length}`;
+
+    }
 });
